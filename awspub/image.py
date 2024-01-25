c5c51c0f2ffd8878663ab19c5f9a731a25a4bfd9
Thomas Bechtold <thomas.bechtold@canonical.com>
2024-01-25 06:38:11 +0100

    refactor: add space between words in log message
    for better readability.

--- awspub/image.py
+++ awspub/image.py
@@ -270,7 +270,7 @@ class Image:
                 if image_info.snapshot_id != snapshot_ids[region]:
                     logger.warning(
                         f"image with name '{self.image_name}' already exists ({image_info.image_id}) "
-                        f"in region {ec2client_region.meta.region_name} but the root device"
+                        f"in region {ec2client_region.meta.region_name} but the root device "
                         f"snapshot id is unexpected (expected {image_info.snapshot_id} but got {snapshot_ids[region]})"
                     )
                 else:
