f7675c44e9d504153fbab3fd57b776f066c76e81
Thomas Bechtold <thomasbechtold@jpberlin.de>
2026-04-15 10:25:17 +0200

    feat(docs): migrate from legacy starter pack to v1.6
    Replace the legacy Sphinx docs starter pack (pre-v1.0) with v1.6.
    This fixes the broken Sphinx venv build CI check by:
    
    - Replacing pyspelling (which depends on html5lib, broken on Python 3.13)
      with Vale for spelling checks
    - Adding apt-get update before apt install in the CI workflow
    - Moving docs infrastructure from repo root into docs/ directory
    
    Key changes:
    - Makefile.sp + root Makefile → single docs/Makefile
    - conf.py + custom_conf.py → standalone docs/conf.py with canonical_sphinx
    - Dynamic .sphinx/build_requirements.py → static docs/requirements.txt
    - canonical-sphinx-extensions → individual extension packages
    - .readthedocs.yaml updated to install awspub via pip for autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -1,30 +1,115 @@
+import datetime
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../"))
+# Make the awspub package importable for autodoc
+sys.path.insert(0, os.path.abspath(".."))
 
-project = 'awspub'
-copyright = '2023, Thomas Bechtold'
-author = 'Thomas Bechtold'
+############################################################
+### Project information
+############################################################
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+project = "awspub"
+author = "Canonical Ltd."
+
+copyright = "%s CC-BY-SA, %s" % (datetime.date.today().year, author)
+
+html_title = project + " documentation"
+
+ogp_site_url = "https://canonical-awspub.readthedocs-hosted.com/"
+ogp_site_name = project
+ogp_image = "https://assets.ubuntu.com/v1/253da317-image-document-ubuntudocs.svg"
+
+html_favicon = "_static/favicon.png"
+
+html_context = {
+    "product_page": "github.com/canonical/awspub",
+    "product_tag": "_static/tag.png",
+    "discourse": "",
+    "mattermost": "",
+    "matrix": "",
+    "github_url": "https://github.com/canonical/awspub",
+    "repo_default_branch": "main",
+    "repo_folder": "/docs/",
+    "sequential_nav": "none",
+    "display_contributors": True,
+    "display_contributors_since": "",
+    "github_issues": "enabled",
+    "author": author,
+    "license": {
+        "name": "GPL-3.0-or-later",
+        "url": "https://github.com/canonical/awspub/blob/main/LICENSE",
+    },
+}
+
+############################################################
+### Template and asset locations
+############################################################
+
+html_static_path = ["_static"]
+templates_path = ["_templates"]
+
+############################################################
+### Extensions
+############################################################
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinxcontrib.autodoc_pydantic',
+    "canonical_sphinx",
+    "notfound.extension",
+    "sphinx_design",
+    "sphinx_tabs.tabs",
+    "sphinxcontrib.jquery",
+    "sphinxext.opengraph",
+    "sphinx_contributor_listing",
+    "sphinx_related_links",
+    "sphinx_roles",
+    "sphinx_terminal",
+    "sphinx_youtube_links",
+    "sphinx_last_updated_by_git",
+    "sphinx.ext.autodoc",
+    "sphinxcontrib.autodoc_pydantic",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = [
+    "doc-cheat-sheet*",
+    ".venv*",
+]
 
+############################################################
+### Link checker exceptions
+############################################################
 
-autodoc_typehints = 'both'
+linkcheck_ignore = [
+    "http://127.0.0.1:8000",
+]
 
-autodoc_pydantic_model_show_json = True
+linkcheck_anchors_ignore_for_url = [r"https://github\.com/.*"]
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+############################################################
+### RST prolog / epilog
+############################################################
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+rst_epilog = """
+.. include:: /reuse/links.txt
+"""
+
+rst_prolog = """
+.. role:: center
+   :class: align-center
+.. role:: h2
+    :class: hclass2
+.. role:: woke-ignore
+    :class: woke-ignore
+.. role:: vale-ignore
+    :class: vale-ignore
+"""
+
+############################################################
+### Additional configuration
+############################################################
+
+disable_feedback_button = False
+
+# Workaround for https://github.com/canonical/canonical-sphinx/issues/34
+if "discourse_prefix" not in html_context and "discourse" in html_context:
+    html_context["discourse_prefix"] = f"{html_context['discourse']}/t/"
